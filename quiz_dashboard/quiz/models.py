1f06b317625b4714aeb1fc8c05af543ea8ae0ca5
Shreyas T C <46958001+shreyastc@users.noreply.github.com>
2024-08-27 17:10:21 +0530

    nav bar update

--- quiz_dashboard/quiz/models.py
+++ quiz_dashboard/quiz/models.py
@@ -1,16 +1,3 @@
-# from django.db import models
-
-# class QuizQuestion(models.Model):
-#     T_ID = models.IntegerField()
-#     question_type = models.CharField(max_length=100)
-#     question = models.TextField()
-#     option_1 = models.CharField(max_length=255)
-#     option_2 = models.CharField(max_length=255)
-#     option_3 = models.CharField(max_length=255)
-#     option_4 = models.CharField(max_length=255)
-#     correct_answer = models.CharField(max_length=255)
-
-
 from django.db import models
 
 class QuizQuestion(models.Model):
